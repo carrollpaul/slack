90f131a2f8afd6147e14b3e5f7233acf019bfbb7
Paul Carroll <57120760+carrollpaul@users.noreply.github.com>
2020-05-28 15:00:04 -0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+# Slack API auth token
+apiToken = 'xoxb-1141859042086-1133857371191-XxUHX8nB1hQGNnB4O3i2udao' 
+# Slack channel ID
+channelId = 'C014B68F5LM'
---
+++ slack.py
@@ -0,0 +1,56 @@
+import requests
+import re
+import config
+from datetime import date
+from bs4 import BeautifulSoup
+
+def getHoliday():
+    month = date.today().strftime('%b') # Get month
+    day = date.today().strftime('%d') # Get day
+    fDate = f'{month.lower()}-{day}' # Format date 
+
+    # Get HTML from home page
+    url = 'https://foodimentary.com/today-in-national-food-holidays/todayinfoodhistorycalenderfoodnjanuary/'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser') # Parse HTML with BeautifulSoup
+
+    # Get the current month URL
+    months = soup.find('ul', id='menu-months', class_='menu') # Isolate the months table
+    monthUrl = months.find('a', href=True, string=month)['href'] # Get the month URL for the current month
+
+    # Get HTML from month page, parse
+    r = requests.get(monthUrl)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    # Find tag with URL that contains formatted date
+    holidayTag = soup.select_one(f'a[href*={fDate}]')
+    # Navigate HTML tree to get chunk that contains National Holiday
+    descendants = holidayTag.parent.parent.descendants
+
+    for child in descendants: # Iterate through all nested tags
+        try:
+            if 'National' in child.string: # If tag contains a string AND the word National, grab the string
+                holiday = child.string
+                break
+        except:
+            continue
+    
+    return holiday
+
+def main():
+    postUrl = 'https://slack.com/api/conversations.setTopic' # Slack API endpoint URL
+
+    holiday = getHoliday() # Get name of today's holiday
+    today = date.today().strftime('%b %d') # Get formatted date
+
+    # POST payload
+    postData = {
+        'token'   : f'{config.apiToken}',
+        'channel' : f'{config.channelId}',
+        'topic'   : f"Happy {holiday} ({today})"
+    }
+
+    p = requests.post(postUrl, data = postData)
+
+if __name__ == '__main__':
+    main()
